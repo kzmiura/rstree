58951b8ca0d3f6e114703df9a32de495b02ad6cc
kzmiura <136795515+kzmiura@users.noreply.github.com>
2025-12-31 18:36:36 +0900

    implement sorted output and summary output.

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,103 @@
+use std::{
+    fs, io,
+    io::prelude::*,
+    path::{Path, PathBuf},
+};
+
 use clap::Parser;
 
-use std::fs;
-use std::io;
-use std::io::prelude::*;
-use std::path::{Path, PathBuf};
-
-#[derive(Debug, Parser)]
-#[command(version, about, long_about = None)]
+#[derive(Debug, clap::Parser)]
+#[command(version)]
 struct Cli {
     dir: PathBuf,
+    #[arg(short, long)]
+    depth: Option<u32>,
     #[arg(short = 'a', long)]
     show_hidden: bool,
-    #[arg(short, long)]
-    depth: Option<usize>,
+}
+
+#[derive(Debug, Default)]
+struct Summary {
+    dirs: u32,
+    files: u32,
 }
 
 fn visit_dirs(
     dir: &Path,
-    lock: &mut impl Write,
-    paddings: &mut String,
-    depth: Option<usize>,
+    handle: &mut dyn Write,
+    prefixes: &mut Vec<&'static str>,
+    depth: u32,
     cli: &Cli,
-) -> io::Result<()> {
-    if dir.is_dir() && depth.is_none_or(|d| d > 0) {
-        let mut entries = fs::read_dir(dir)?
-            .filter_map(|res| {
-                res.inspect_err(|e| eprintln!("{}: {}", dir.display(), e))
-                    .ok()
-            })
-            .filter(|e| cli.show_hidden || !e.file_name().as_encoded_bytes().starts_with(b"."))
-            .peekable();
-        while let Some(entry) = entries.next() {
-            let (padding, prefix) = if entries.peek().is_some() {
-                ("|   ", "|-- ")
-            } else {
-                ("    ", "`-- ")
-            };
-            let file_name = entry.file_name();
-            writeln!(
-                lock,
-                "{}{}{}",
-                paddings,
-                prefix,
-                file_name.display()
-            )?;
-
-            let path = entry.path();
-            if path.is_dir() {
-                paddings.push_str(padding);
-                if let Err(e) = visit_dirs(&path, lock, paddings, depth.map(|d| d - 1), cli) {
-                    eprintln!("{}: {}", path.display(), e);
+) -> io::Result<Summary> {
+    let Cli {
+        show_hidden,
+        depth: max_depth,
+        ..
+    } = *cli;
+    let mut summary = Summary::default();
+    if max_depth.is_none_or(|max| depth < max) {
+        match fs::read_dir(dir) {
+            Ok(entries) => {
+                let mut v: Box<[_]> = entries
+                    .filter_map(|r| r.inspect_err(|e| eprintln!("{}", e)).ok())
+                    .filter(|e| {
+                        show_hidden || e.file_name().to_str().is_none_or(|n| !n.starts_with('.'))
+                    })
+                    .collect();
+                v.sort_by_key(|e| e.file_name());
+                for (i, entry) in v.iter().enumerate() {
+                    let (prefix, connector) = if i + 1 == v.len() {
+                        ("   ", "\u{2514}\u{2500} ")
+                    } else {
+                        ("\u{2502}  ", "\u{251C}\u{2500} ")
+                    };
+                    let path = entry.path();
+                    let file_name = entry.file_name();
+                    writeln!(
+                        handle,
+                        "{}{}{}",
+                        prefixes.concat(),
+                        connector,
+                        file_name.display(),
+                    )?;
+                    if path.is_dir() {
+                        summary.dirs += 1;
+                        prefixes.push(prefix);
+                        let Summary { dirs, files } =
+                            visit_dirs(&path, handle, prefixes, depth + 1, cli)?;
+                        summary.dirs += dirs;
+                        summary.files += files;
+                        prefixes.pop();
+                    } else if path.is_file() {
+                        summary.files += 1;
+                    }
                 }
-                paddings.truncate(paddings.len() - padding.len());
+            }
+            Err(e) => {
+                eprintln!("{}: {}", dir.display(), e);
             }
         }
     }
-    Ok(())
+    Ok(summary)
 }
 
 fn main() -> io::Result<()> {
     let cli = Cli::parse();
-
-    let mut lock = io::stdout().lock();
-    writeln!(lock, "{}", cli.dir.display())?;
-    visit_dirs(&cli.dir, &mut lock, &mut String::new(), cli.depth, &cli)?;
-
+    let Cli { dir, .. } = &cli;
+    let mut handle = io::stdout().lock();
+    writeln!(handle, "{}", dir.display())?;
+    let Summary { dirs, files } = visit_dirs(dir, &mut handle, &mut vec![], 0, &cli)?;
+    writeln!(
+        handle,
+        "\n{} {}, {} {}",
+        dirs,
+        if dirs == 1 {
+            "directory"
+        } else {
+            "directories"
+        },
+        files,
+        if files == 1 { "file" } else { "files" }
+    )?;
     Ok(())
 }
